406a3e66dcd601a053ee3c4e5f6e0c04b7ad952c
moumar1 <147613636+moumar1@users.noreply.github.com>
2024-10-25 14:37:01 +0000

    Addition to requirements.txt

--- Python Automation/app.py
+++ Python Automation/app.py
@@ -2,4 +2,12 @@ import openpyxl as xl
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
 cell = sheet['a1']
-print(cell.value)
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+    print(corrected_price)
+
+wb.save('transactions2.xlsx')
